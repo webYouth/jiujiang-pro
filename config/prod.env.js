08601fcdeea44fa50c6c9dbccaa28078aee3369b
webCoder <nicefish@dingtalk.com>
2019-06-11 19:19:48 +0800

    update by last time

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,6 +1,6 @@
 'use strict'
 module.exports = {
   NODE_ENV: '"production"',
-  // BASE_API: '"https://house.rangni.cn/api"',
-  BASE_API: '"api"',
+  BASE_API: '"https://house.rangni.cn/api"',
+  // BASE_API: '"api"',
 }
